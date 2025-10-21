cf9a21d7c01ace255e8ee680f64846272d68ef1e
karim313 <kareemmedhat03@gmail.com>
2025-10-21 04:21:08 +0300

    fix case sensitivity issue in CSS import

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./Contact.css";
+import "./contact.css";
 import LineComponent from "../LineComponent/LineComponent";
 import { useEffect } from 'react'
 
